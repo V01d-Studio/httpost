11d95cc4b5c25a4f45c1889d27b8057e88279191
Sihan Tawsik <sihantawsik@gmail.com>
2023-11-12 22:00:42 +0600

    add: json highlighting

--- script/engine.js
+++ script/engine.js
@@ -4,7 +4,6 @@ function makeRequest() {
   const body = document.getElementById("body").value;
   const headersInput = document.getElementById("headers").value;
 
-  // Parse headers input into key-value pairs
   const headers = {};
   headersInput.split(",").forEach((header) => {
     const [key, value] = header.split(":");
@@ -17,25 +16,70 @@ function makeRequest() {
     method: method,
     headers: {
       "Content-Type": "application/json",
-      ...headers, // Include custom headers
+      ...headers,
     },
     body: method === "GET" ? undefined : body,
   })
     .then((response) => response.json())
     .then((data) => {
-      document.getElementById("response").textContent = JSON.stringify(
-        data,
-        null,
-        2
-      );
+      const responseElement = document.getElementById("response");
+      responseElement.textContent = JSON.stringify(data, null, 2);
+      highlightJSON(responseElement);
     })
     .catch((error) => {
-      document.getElementById("response").textContent =
-        "Error: " + error.message;
+      const responseElement = document.getElementById("response");
+      responseElement.textContent = "Error: " + error.message;
+      responseElement.classList.remove("json-highlight");
     });
 }
+
+document
+  .getElementById("jsonInput")
+  .addEventListener("input", () =>
+    highlightJSON(document.getElementById("jsonOutput"))
+  );
 
 function toggleDarkMode() {
   const container = document.querySelector(".container");
   container.classList.toggle("dark-mode");
+  const jsonOutput = document.getElementById("response");
+  jsonOutput.classList.remove("json-highlight");
+  highlightJSON(jsonOutput);
+}
+
+function highlightJSON(element) {
+  try {
+    const parsedJSON = JSON.parse(element.textContent);
+    element.innerHTML = syntaxHighlight(parsedJSON);
+    element.classList.add("json-highlight");
+  } catch (error) {
+    element.classList.remove("json-highlight");
+  }
+}
+
+function syntaxHighlight(json) {
+  if (typeof json !== "string") {
+    json = JSON.stringify(json, undefined, 2);
+  }
+
+  json = json.replace(
+    /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+    function (match) {
+      let cls = "number";
+      if (/^"/.test(match)) {
+        if (/:$/.test(match)) {
+          cls = "key";
+        } else {
+          cls = "string";
+        }
+      } else if (/true|false/.test(match)) {
+        cls = "boolean";
+      } else if (/null/.test(match)) {
+        cls = "null";
+      }
+      return '<span class="' + cls + '">' + match + "</span>";
+    }
+  );
+
+  return json;
 }
