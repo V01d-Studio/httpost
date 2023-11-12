d22c69096bb24658dd7e38e42d017d16ad8e13b2
Sihan Tawsik <sihantawsik@gmail.com>
2023-11-12 23:27:36 +0600

    remove: unnecessary code

--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
-
-func headers(w http.ResponseWriter, req *http.Request) {
-
-    for name, headers := range req.Header {
-        for _, h := range headers {
-            fmt.Fprintf(w, "%v: %v\n", name, h)
-        }
-    }
-}
 
 func main() {
 	fs := http.FileServer(http.Dir("./"))
